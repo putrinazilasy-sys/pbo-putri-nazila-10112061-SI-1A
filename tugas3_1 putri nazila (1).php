4fe1adbfef130b313b52ec46c3bdf8aa5f30c3a6
putrinazilasy-sys <putrinazilasy@gmail.com>
2026-02-24 07:57:54 +0700

    Add files via upload

---
+++ tugas3_1 putri nazila (1).php
@@ -0,0 +1,38 @@
+<?php
+class Kendaraan
+{
+    public $jumlahRoda = 4;
+    public $warna;
+    public $bahanBakar = "Premium";
+    public $harga = 100000000;
+    public $merek;
+    public $tahunPembuatan = 2004;
+
+    public function statusHarga()
+    {
+        if ($this->harga > 50000000) {
+            $status = "Harga Kendaraan Mahal";
+        } else {
+            $status = "Harga Kendaraan Murah";
+        }
+        return $status;
+    }
+
+    public function statusSubsidi()
+    {
+        if ($this->tahunPembuatan < 2005 && $this->bahanBakar == "Premium") {
+            $status = "DAPAT SUBSIDI";
+        } else {
+            $status = "TIDAK DAPAT SUBSIDI";
+        }
+        return $status;
+    }
+}
+
+// instansiasi objek
+$objekKendaraan = new Kendaraan();
+
+echo "Jumlah roda: " . $objekKendaraan->jumlahRoda . "<br>";
+echo "Status harga: " . $objekKendaraan->statusHarga() . "<br>";
+echo "Status subsidi: " . $objekKendaraan->statusSubsidi();
+?>
